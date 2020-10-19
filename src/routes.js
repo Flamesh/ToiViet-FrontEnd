f0f4612e4a9f823eaa040ccd4de5e8e2648a8778
unknown <tuyenkbhb@gmail.com>
2020-10-19 22:39:42 +0700

    tiep tuc the loai bai viet

--- src/routes.js
+++ src/routes.js
@@ -1,38 +1,33 @@
 import { lazy } from "react";
 
 // @material-ui/icons
-import Dashboard from "@material-ui/icons/Dashboard";
-import Person from "@material-ui/icons/Person";
-import Notifications from "@material-ui/icons/Notifications";
-
-// core components/views for RTL layout
-
 const dashboardRoutes = [
   {
     path: "/dashboard",
     name: "Dashboard",
-    icon: Dashboard,
     component: lazy(() => import("views/Dashboard/Dashboard")),
   },
   {
     path: "/bai-viet",
     name: "Bai viet",
-    icon: Notifications,
+
     component: lazy(() => import("views/Detail/DetailArticle")),
   },
   {
     path: "/nguoi-dung",
     name: "Nguoi dung",
-    icon: Notifications,
-    component: lazy(() => import("views/Profile/Profile"))
-
+    component: lazy(() => import("views/Profile/Profile")),
   },
   {
     path: "/top-tac-gia",
     name: "Top tac gia",
-    icon: Notifications,
-    component: lazy(() => import("views/TopUser/TopUser"))
-  }
+    component: lazy(() => import("views/TopUser/TopUser")),
+  },
+  {
+    path: "/the-loai",
+    name: "The Loai",
+    component: lazy(() => import("views/TypeArticle/TypeArticle")),
+  },
 ];
 
 export default dashboardRoutes;
--- src/views/TypeArticle/TypeArticle.js
+++ src/views/TypeArticle/TypeArticle.js
@@ -1,20 +1,43 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import cover from "assets/img/cover.jpeg";
+import { Fab, Grid, Typography } from '@material-ui/core'
+import React from 'react'
+import cover from "../../assets/img/cover.jpeg"
 
-export const TypeArticle = () => {
+import './TypeArticle.scss'
+
+const nameType = "Quan điểm - tranh luận"
+export default function TypeArticle() {
   return (
-    <div>
+    <div className="root-type-article">
       <div className="header">
-        <img src={cover} alt="cover" />
-        <div>Khoa học công nghệ</div>
+        <img src={cover} alt="cover" className="cover" />
+       
+        <div className="text-box">
+        <div className='name-type'>
+          {nameType}
+        </div>
+          <Fab  variant="extended"
+        size="medium"
+        color="primary"
+        aria-label="add"
+        className="fab"
+        >
+          Theo dõi
+        </Fab>
+        </div>
+      </div>
+      <div>
+        <Grid container alignContent='center' alignItems="center" justify="center">
+        <Grid item xs={0} sm={0} md={2} lg={2} xxl={2} >
+          a
+        </Grid>
+        <Grid item xs={12} sm={12} md={7} lg={7} xxl={7}>
+          b
+        </Grid>   
+        <Grid item xs={0} sm={0} md={3} lg={3} xxl={3}>
+          
+          c</Grid> 
+           </Grid>
       </div>
     </div>
-  );
-};
-
-const mapStateToProps = (state) => ({});
-
-const mapDispatchToProps = {};
-
-export default connect(mapStateToProps, mapDispatchToProps)(TypeArticle);
+  )
+}
